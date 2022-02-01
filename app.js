13135144ecd69bbd0965f5039132fef796184fa1
Pri2804 <priscilanahirf@hotmail.com>
2022-02-01 16:52:43 -0300

    Creación de la estructura Node.js+Express

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, './public')))
+
+app.listen(3007, () => console.log('Servidor corriendo en puerto 3007'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+});
