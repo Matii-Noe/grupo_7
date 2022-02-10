86f1219e3114f9e269a8dbcdb9393ec25464db6d
Mati <matiasnoe2002@gmail.com>
2022-02-10 11:28:03 -0300

    Agregando la ruta de productCart en el archivo app.js

--- app.js
+++ app.js
@@ -9,4 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
-/* Link del error que tengo https://gist.github.com/claraj/e5563befe6c2fb108ad0efb6de47f265 */ 
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+});
