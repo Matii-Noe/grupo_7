dd45cf7a29fd0a377c44f5adc017a0dbda1ed4f8
Mati <matiasnoe2002@gmail.com>
2022-02-10 11:20:33 -0300

    Agregando código a los archivos productCart.html y productCart.css y la carpeeta css

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ app.listen(3007, () => console.log('Servidor corriendo en puerto 3007'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 });
+
+/* Link del error que tengo https://gist.github.com/claraj/e5563befe6c2fb108ad0efb6de47f265 */ 
