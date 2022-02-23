748e189b94458734ce1f70888ebf3581118ab9dc
Mati <matiasnoe2002@gmail.com>
2022-02-23 20:10:19 -0300

    Modificando el app.get del archivo  app.js

--- src/app.js
+++ src/app.js
@@ -7,13 +7,13 @@ app.use(express.static(path.resolve(__dirname, './public')))
 
 app.listen(3007, () => console.log('Servidor corriendo en puerto 3007'));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.render('index')
 });
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'))
+    res.render('register')
 })
 
 app.get('/productCart', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+    res.render('productCart.html')
 });
