0ad4cf46e83457ee10f38ddd1fe93fc52a9d04c1
Pri2804 <priscilanahirf@hotmail.com>
2022-03-21 17:55:05 -0300

    Algunos cambios al header

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
+router.get('/edit', productsController.edit);
 
-router.get('/', productsController.edit); 
+/*router.get('/', productsController.edit); 
 
 /*
 
@@ -13,7 +14,6 @@ router.get('/productCart', productsController.productCart);
 
 router.get('/productDetail', productsController.productDetail);  
 
-router.get('/edit', productsController.edit);
 
 router.get('/create', productsController.create);*/
 
