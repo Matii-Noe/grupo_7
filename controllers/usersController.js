1e51a1a583f926a35a43be1fb3c6680566b7ff26
MatiasMartin1 <matiasmartinnnn@gmail.com>
2022-04-13 19:25:58 -0300

    processRegister modificado

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,6 +4,7 @@ const path = require('path');
 const usersFilePath = path.join(__dirname,'../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const User = require('../models/User');
+const {validationResult}=require('express-validator');
 
 const controller = {
 	register: (req, res) => {
