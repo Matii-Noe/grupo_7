828a4c5661a73958e9a101aec0be8b8ef4fa4b14
Matias Noe <matiasnoe2002@gmail.com>
2022-06-02 11:14:03 -0300

    Agregando un espacio aleatorio para probar que git se haya vinculado correctamente

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,6 +6,7 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
+
 const controller = {
 	index: (req, res) => {
 		db.Product.findAll()
