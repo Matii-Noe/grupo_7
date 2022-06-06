6a639058b1225b3a9b3fbf4d28e73df419f3af9b
MatiasMartin1 <matiasmartinnnn@gmail.com>
2022-06-06 20:11:09 -0300

    validaciones

--- middlewares/validacionesRegistro.js
+++ middlewares/validacionesRegistro.js
@@ -4,14 +4,14 @@ const {check} = require('express-validator');
 let validateRegister = [
     check('name')
         .notEmpty().withMessage('Debes completar este campo')
-        .isLength({max: 100}),
+        .isLength({min: 2}),
     check('email')
         .notEmpty().withMessage('Debes completar este campo')
         .isEmail().withMessage('Debes ingresar un email valido'),
-
     check('password')
         .notEmpty().withMessage('Debes completar este campo')
         .isLength({min: 8, max:20}).withMessage('Este campo debe tener entre 6 y 15 caracteres')
+        .has().uppercase().lowercase().number().withMessage('Debe contener al menos una mayúscula, minúscula y un número'),
 ];
 
 module.exports = validateRegister;
